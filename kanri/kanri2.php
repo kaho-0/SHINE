6af7b0cbfb5396ed93f4e55144d3b3f222def789
Eula0313 <146159199+Eula0313@users.noreply.github.com>
2023-10-25 14:18:32 +0900

    Add files via upload

---
+++ kanri/kanri2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ユーザー管理2</title>
+</head>
+<body class="c">
+<style>
+.cnt {
+    text-align: center;
+    font-size: 24px;
+}
+
+.radio_cnt {
+    display: flex;
+    align-items: center;
+    font-size: 24px;
+}
+
+.radio {
+    margin-right: 220px; 
+    margin-left:50px;
+}
+.kuhaku{
+    margin-left:360px; 
+}
+.ku {
+    margin-left:160px; 
+}
+
+</style>
+    <div class="header-logo">
+        <h1 class="shine">SHINE</h1>
+    </div>
+    <div class="cnt">
+        <h1>ユーザー管理</h1>
+        <hr>
+        
+            <h2>ログインID<div class="ku">メールアドレス</h2></div>
+        </div>
+        <hr>
+        <div class="radio_cnt">
+            <div class="radio">
+                <input type="radio" name="seni" style="transform: scale(2.0);">
+            </div>
+            ログインID <div class="kuhaku">メールアドレス</div>
+        </div>
+        <hr>
+        <div class="radio_cnt">
+            <div class="radio">
+                <input type="radio" name="seni" style="transform: scale(2.0);">
+            </div>
+            ログインID <div class="kuhaku">メールアドレス</div>
+        </div>
+        <hr>
+</body>
+</html>
---
+++ kanri/rireki.php
@@ -0,0 +1,36 @@
+<?php require 'header.php' ; ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>購入履歴</title>
+</head>
+    <body class="a">
+        <style>
+        .shohin {
+            display: inline-block; 
+            vertical-align: top; 
+            margin-left: 20px; 
+            font-size: 24px;
+        }
+        </style>
+        <h1 calss="rireki">○○様の注文履歴</h1>
+    <hr>
+        <h1 class="kensu">件数：○件</h1>
+        <img src="./img/favo.png" width="200" height="200">
+        <div class="shohin">
+            <p>商品名</P>
+            <p>カラー/サイズ</p>
+            <p>￥○○○○　　　　　　数量：○</p>
+        </div>
+    <hr>
+        <img src="./img/favo.png" width="200" height="200">
+        <div class="shohin">
+            <p>商品名</P>
+            <p>カラー/サイズ</p>
+            <p>￥○○○○　　　　　　数量：○</p>
+        </div>
+    <hr>
+    </body>
+</html>
