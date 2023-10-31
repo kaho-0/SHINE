6bdcb1c3be92e9e419720586653b1ef7cfe6fe01
kei0505 <119839500+kei0505@users.noreply.github.com>
2023-10-31 11:06:22 +0900

    css、menu変更

--- kaihatu_k/menu.php
+++ kaihatu_k/menu.php
@@ -1,16 +1,7 @@
-<link rel="stylesheet" href="css/menu_k.css">
+<link rel="stylesheet" href="css/menu.css">
 <div class="menu">
-    <div class="menu-ber">
-        <div class="menu-link">
-            <a href="product.php" class="sentaku">全て</a>
-            <a href="favorite-show.php" class="sentaku">メンズ</a>
-            <a href="history.php" class="sentaku">レディース</a>
-            <a href="cart-show.php" class="sentaku">シニア</a>
-            <a href="purchase-input.php" class="sentaku">キッズ</a>
-        </div>
-    </div>
     <div class="main">
-        <div class="left-menu">
+        <div class="sidebar">
             <div class="category">
                 <p class="category-name">カテゴリー</p>
                 <ul>
@@ -23,19 +14,22 @@
             </div>
         </div>
         <div class="main-contents">
+            <div class="menu-ber">
+                <div class="menu-link">
+                    <a href="product.php" class="sentaku">全て</a>
+                    <a href="favorite-show.php" class="sentaku">メンズ</a>
+                    <a href="history.php" class="sentaku">レディース</a>
+                    <a href="cart-show.php" class="sentaku">シニア</a>
+                    <a href="purchase-input.php" class="sentaku">キッズ</a>
+                </div>
+            </div>
             <div class="shohin-img">
                 <?php
-                echo '<table>';
-                $pdo = new PDO($connect, USER, PASS);
-                $sql = $pdo->query('select * from shohin');
-                foreach ($sql as $row) {
-                    echo '<tr>';
-                    echo '<td>';
-                    echo '<a href="./kaihatu/shohin1/shosai.php?id=', $id, '"><img src="', $row['img_pass'], '"></a>';
-                    echo '</td>';
-                    echo '</tr>';
+                for ($i = 0; $i < 3; $i++) {
+                    echo '<img class="img" src="./img/favo.png">
+                    <img class="img" src="./img/favo.png">
+                    <img class="img" src="./img/favo.png"><br>';
                 }
-                echo '</table>';
                 ?>
             </div>
         </div>
