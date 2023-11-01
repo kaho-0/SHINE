733c3f4fb3b2c4a3a780538d39c529f0ca9faf74
sho0515 <146160851+sho0515@users.noreply.github.com>
2023-11-01 14:24:09 +0900

    連携したやつです

---
+++ db連携済み/login-output.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<?php require 'db-connect.php'; ?> 
+<?php require 'header.php'; ?>
+<?php
+unset($_SESSION['client']);
+$pdo = new PDO($connect, USER, PASS);
+$sql = $pdo->prepare('select * from client where ID=?');
+$sql->execute([$_POST['ID']]);
+
+if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+    if ($_POST['PW'] == $row['PW']) {
+        $_SESSION['client'] = [
+            'ID' => $row['ID'], 'PW' => $row['PW'], 'name' => $row['name'], 'BD' => $row['BD'],
+            'address' => $row['address'], 'mail' => $row['mail']
+        ];
+        echo 'いらっしゃいませ、', $_SESSION['client']['name'], 'さん。';
+    } else {
+        echo 'ログイン名またはパスワードが違います。';
+    }
+} else {
+    echo 'ログイン名またはパスワードが違います。';
+}
+?>
+<?php require 'footer.php'; ?>
