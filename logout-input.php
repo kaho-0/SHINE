044166a0569ea6db807453e470037a82a8cfa33b
daichan115115 <146160249+daichan115115@users.noreply.github.com>
2023-11-07 09:55:23 +0900

    Add files via upload

---
+++ logout-input.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/styles.css"> 
+</head>
+<body>
+<div class="container">
+    <p class="message">ログアウトしますか？</p>
+    <a class="logout-link" href="logout-output.php">ログアウト</a>
+</div>
+<?php require 'footer.php'; ?>
+</body>
+</html>
