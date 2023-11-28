2773bd5367474778974b180ccad8906552be6127
oonishi0321 <146159310+oonishi0321@users.noreply.github.com>
2023-11-28 10:05:24 +0900

    Add files via upload

--- 完成済み/mypage/toroku.php
+++ 完成済み/mypage/toroku.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require '../header.php';
-require '../db-connect.php'; 
-
+require 'header.php';
+require 'db-connect.php';
+ 
 if (isset($_SESSION['client'])) {
     $client_id = $_SESSION['client']['ID'];
-
+ 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user_id = $_SESSION['client']['ID'];
         $stmt = $pdo->prepare("UPDATE client SET name=?, address=?, mail=?, PW=? WHERE ID=?");
@@ -13,29 +13,26 @@ if (isset($_SESSION['client'])) {
             $_POST['name'],
             $_POST['address'],
             $_POST['mail'],
-            $_POST['PW'],
-            $user_id
+            $_POST['PW'],/*  */
+            $user_id,
         ]);
     }
-
+ 
     $sql = $pdo->prepare('SELECT * FROM client WHERE ID = ?');
     $sql->execute([$client_id]);
-    $row = $sql->fetch(PDO::FETCH_ASSOC);
-    $_SESSION['client'] = $row;
-
-    if ($row) {
+ 
+    if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
         echo '
         <!DOCTYPE html>
         <html lang="ja">
         <head>
             <meta charset="UTF-8">
-            <link rel="stylesheet" href="./css/style3.css">
+            <link rel="stylesheet" href="./css/style1.css">
             <title>登録情報</title>
         </head>
         <body>
-        <div class="container">
             <h1>', $row['name'], '様の登録情報</h1>
-            <div class="info-section">
+            <div>
                 <p>名前: ', $row['name'], '</p>
                 <hr>
                 <p>ご住所: ', $row['address'], '</p>
@@ -47,25 +44,31 @@ if (isset($_SESSION['client'])) {
                 <p>メールアドレス: ', $row['mail'], '</p>
                 <hr>
                 <p>パスワード: ', $row['PW'], '</p>
-                <hr>
-                <p>カード番号: ', $row['card_num'], '</p>
-                <hr>
-                <p>有効期限: ', $row['card_date'], '</p>
-                <hr>
+                <hr>';
+                
+        
+                if(isset($row['card_num']) && isset($row['card_date'])) {
+                    echo '
+                    <p>カード番号: ', $row['card_num'], '</p>
+                    <hr>
+                    <p>有効期限: ', $row['card_date'], '</p>
+                    <hr>';
+                } else {
+                    echo '<p>カード情報は登録されていません。</p>';
+                }
+                
+                echo '
                 <p>現在の住所: ', $row['address'], '</p>
                 <hr>
-                </div>
-                <div class="link-section">
+            </div>
+ 
+            <div>
                 <a href="rireki.php?client_id=', $row['ID'], '">注文履歴</a>
                 <a href="toroku1.php">登録情報を変更する</a>
-                </div>
-                <div class="logout-form">
                 <form action="login-input.php" method="post">
                     <input type="submit" value="ログアウト">
                 </form>
             </div>
-            </div>
-
         </body>
         </html>';
     } else {
