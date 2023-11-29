49c885c9814169d176db64f2ddac6bc139f6898b
Eula0313 <146159199+Eula0313@users.noreply.github.com>
2023-11-29 14:59:15 +0900

    エラー文の追加

--- 完成済み/login-output.php
+++ 完成済み/login-output.php
@@ -1,7 +1,8 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?> 
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+
+
 <?php
 unset($_SESSION['client']);
 $pdo = new PDO($connect, USER, PASS);
@@ -12,8 +13,12 @@ if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
     if ($_POST['PW'] == $row['PW']) {
         $_SESSION['client'] = [
             'ID' => $row['ID'], 'PW' => $row['PW'], 'name' => $row['name'], 'BD' => $row['BD'],
-            'address' => $row['address'], 'mail' => $row['mail'] ,'tell'=>$row['tell']
+            'address' => $row['address'], 'mail' => $row['mail'] ,'yubin'=>$row['yubin'] ,'tell'=>$row['tell']
         ];
+
+        echo '<h1 class="login-success">いらっしゃいませ!',$_SESSION['client']['name'],'様</h1>';
+
+        require 'menu.php';
     } else {
         echo 'ログイン名またはパスワードが違います。';
     }
@@ -22,3 +27,11 @@ if ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
 }
 ?>
 <?php require 'footer.php'; ?>
+
+
+<style>
+    h1.login-success{
+        margin: 0 auto;
+        text-align: center;
+    }
+</style>
